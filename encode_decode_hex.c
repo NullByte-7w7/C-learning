f7fd95e04ef372b738ed1dfb877b6f24319e8de0
NullByte <121623691+NullByte-7w7@users.noreply.github.com>
2024-12-25 17:26:58 -0300

    Create encode_decode_hex.c

---
+++ encode_decode_hex.c
@@ -0,0 +1,28 @@
+// encode string to hexadecimal and hexadecimal to string
+//NullByte
+#include <stdio.h>
+#include <stdlib.h>
+
+void *encode_decode(char *encode){
+	printf("[$] Encoding string -> ");
+	for (int i = 0; i < sizeof(encode); i++){
+		printf("\\0x0%x",encode[i]);
+	printf("\n[#] Decoding String -> ");
+		for (i; i < sizeof(encode);i++){
+			printf("%c",encode[i]);
+		}
+	}
+}
+
+
+int main(void){
+
+
+	char encodes[100]; 
+
+	printf("[!] PUT YOUR STRING -> ");
+	fgets(encodes,100,stdin);
+	encode_decode(encodes);
+
+	return 0;
+}
